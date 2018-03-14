65ccc5b91347fa0c0a5d32ec0ac9909dc7a9ddda
田鑫 <844577216@qq.com>
2018-03-14 09:56:59 +0800

    配置常量文件文件

--- framework_core/core/CodeIgniter.php
+++ framework_core/core/CodeIgniter.php
@@ -1,75 +1,32 @@
 <?php
 /**
- * CodeIgniter
- *
- * An open source application development framework for PHP
- *
- * This content is released under the MIT License (MIT)
- *
- * Copyright (c) 2014 - 2018, British Columbia Institute of Technology
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package	CodeIgniter
- * @author	EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2018, British Columbia Institute of Technology (http://bcit.ca/)
- * @license	http://opensource.org/licenses/MIT	MIT License
- * @link	https://codeigniter.com
- * @since	Version 1.0.0
- * @filesource
+ * Created by PhpStorm.
+ * User: tianxin
+ * Date: 2018/3/13
+ * Time: 16:35
  */
-defined('BASEPATH') OR exit('No direct script access allowed');
+//本文件是系统初始化文件
+//没有从入口脚本访本文件就不能访问
+defined('COREPATH') OR exit('No direct script access allowed');
 
 /**
- * System Initialization File
- *
- * Loads the base classes and executes the request.
- *
- * @package		CodeIgniter
- * @subpackage	CodeIgniter
- * @category	Front-controller
- * @author		EllisLab Dev Team
- * @link		https://codeigniter.com/user_guide/
- */
-
-/**
- * CodeIgniter Version
- *
- * @var	string
- *
+ *本系统使用CI框架版本
  */
 	const CI_VERSION = '3.1.7';
 
 /*
  * ------------------------------------------------------
- *  Load the framework constants
+ *  加载本应用配置文件,首先加载常量配置文件
  * ------------------------------------------------------
  */
-	if (file_exists(APPPATH.'config/'.ENVIRONMENT.'/constants.php'))
+	if (file_exists(FRONTENDPATH.'config/'.ENVIRONMENT.'/constants.php'))
 	{
-		require_once(APPPATH.'config/'.ENVIRONMENT.'/constants.php');
+		require_once(FRONTENDPATH.'config/'.ENVIRONMENT.'/constants.php');
 	}
 
-	if (file_exists(APPPATH.'config/constants.php'))
+	if (file_exists(FRONTENDPATH.'config/constants.php'))
 	{
-		require_once(APPPATH.'config/constants.php');
+		require_once(FRONTENDPATH.'config/constants.php');
 	}
 
 /*
--- frontend/web/index.php
+++ frontend/web/index.php
@@ -12,8 +12,25 @@
  */
 defined('ENVIRONMENT') or define('ENVIRONMENT','development');
 //项目根目录文件夹
-defined('BASEDIR') or define('BASEDIR',dirname(pathinfo(__DIR__,PATHINFO_DIRNAME)));
-defined('FRONTENDDIR') or define('FRONTENDDIR',pathinfo(__DIR__,PATHINFO_DIRNAME));
+define('BASEDIR',dirname(pathinfo(__DIR__,PATHINFO_DIRNAME)));
+//本应用根目录
+/**
+ * pathinfo()获取路径信息
+ *Array
+ * (
+ *   [dirname] => D:\wamp64\www\CI-XIN
+ *   [basename] => index.php
+ *   [extension] => php
+ *   [filename] => test
+ * )
+ */
+define('FRONTENDDIR',pathinfo(__DIR__,PATHINFO_DIRNAME));
+//本文件名
+/**
+ * basename(path,suffix)
+ * suffix是去掉的文件名后缀,默认不去
+ */
+define('SELF',basename(__FILE__));
 /******************************************************************/
 switch (ENVIRONMENT) {//CI为了兼顾5.3以下版本做的一些环境配置
     //以下为页面显示报错级别
@@ -87,7 +104,93 @@ if(defined('STDIN')){
  * 确认框架核心代码文件夹和本应用程序文件夹的路径
  */
 if($_temp=realpath($framework_core_folder)!==FALSE){
+    //文件夹路径存在
     $framework_core_folder=$framework_core_folder.DIRECTORY_SEPARATOR;
 }else{
-
+    $framework_core_folder = strtr(rtrim($framework_core_folder, '/\\'),'/\\',DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
 }
+
+//文件夹不存在
+if(!is_dir($framework_core_folder)){
+    /* 向客户端发送原始的 HTTP 报头 */
+    header('HTTP/1.1 503 Service Unavailable.',true,503);
+    echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+    exit(3); //3仅仅表示一个状态,是因为配置有问题导致程序退出
+}
+
+/**
+ *CI核心代码路径
+ */
+define('COREPATH',$framework_core_folder);
+/**
+ * 项目路径
+ */
+define('BASEPATH',BASEDIR.DIRECTORY_SEPARATOR);
+
+/**
+ * 确认本应用程序文件夹的路径,同上
+ */
+if(is_dir($application_folder)){
+    if($_temp=realpath($application_folder)!==FALSE){
+        $application_folder=$application_folder.DIRECTORY_SEPARATOR;
+    }else{
+        $application_folder=strtr(rtrim($application_folder,'/\\'),'/\\',DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
+    }
+}else{
+    header('HTTP/1.1 503 Service Unavailable',true,503);
+    echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+    exit(3);
+}
+
+/**
+ *应用文件夹路径
+ */
+define('FRONTENDPATH',$application_folder);
+
+/**
+ * 确认视图路径
+ */
+if(!isset($view_folder)&&empty($view_folder)&&is_dir(FRONTENDPATH.'views'.DIRECTORY_SEPARATOR)){
+    //如果$view_folder不存在或为空,在应用文件夹下有
+    $view_folder=FRONTENDDIR.'views'.DIRECTORY_SEPARATOR;
+}else if(is_dir($view_folder)){
+    if($_temp=realpath($view_folder)!==FALSE){
+        $view_folder=$view_folder.DIRECTORY_SEPARATOR;
+    }else{
+        $view_folder=strtr(rtrim($view_folder,'/\\'),'/\\',DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
+    }
+}else{
+    header('HTTP/1.1 503 Service Unavailable',true,503);
+    echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+    exit(3);
+}
+
+/**
+ * 定义视图常量
+ */
+define('VIEWPATH',$view_folder);
+
+
+/**
+ * 本页是应用入口页,干了这么几件事
+ * 1.定义环境状态常量
+ * 2.定义项目根目录与本应用根目录文件夹与本入口文件全名
+ * 3.定义不同代码运行环境下的报错等级
+ * 4.定义CI核心代码文件夹,应用程序文件夹,视图文件夹
+ * 5.判断php是否是以cli模式运行(是,切换文件夹到根目录)
+ * 6.确认CI核心代码文件夹路径
+ * 7.定义CI核心代码文件路径,根目录路径常量
+ * 8.确认本应用文件夹路径
+ * 9.定义本应用文件夹路径常量
+ * 10.定义视图文件夹路径
+ * 11.定义视图文件夹路径常量
+ * 12.载入CodeIgniter核心文件
+ */
+
+require_once COREPATH.'core/CodeIgniter.php';
+
+
+
+
+
+
